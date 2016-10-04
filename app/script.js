ad4c067ce1fe0ec322961e4e0f0e04a31eb45f77
Tim <t.schuitemaker@gen25.com>
2016-10-04 13:58:21 +0200

    styling

--- app/script.js
+++ app/script.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {   
 	var sfID = convertId(window.location.pathname.substring(1,16));
-
 	var t1='<div class="brandTertiaryBrd pbSubheader tertiaryPalette"><h3>Extra information<span class="titleSeparatingColon">:</span></h3></div>';
 	var t2='<div class="pbSubsection"><table class="detailList" border="0" cellpadding="0" cellspacing="0"><tbody><tr><td class="last labelCol">ID 18 chars</td><td class="dataCol last col02" id="18char">' + sfID + '</td><td>&nbsp;</td><td>&nbsp;</td></tr></tbody></table>';
+
 	$(".bDetailBlock .pbBody").append(t1 + t2);
 });
 
-
 //http://salesforce.stackexchange.com/questions/27686/how-can-i-convert-a-15-char-id-value-into-an-18-char-id-value
 function convertId(id) { 
-	 var i, j, flags, alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ012345";
-
+	var i, j, flags, alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ012345";
     if (id === null) return id;
     id = id.replace(/\W/g, "");
     if (id.length != 15) { return id; }
