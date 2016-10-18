47b20712de4c323eb5b5997091af5b189aef3974
Tim <t.schuitemaker@gen25.com>
2016-10-18 08:46:28 +0200

    Prettyfied code

--- app/script.js
+++ app/script.js
@@ -1,17 +1,32 @@
-$(document).ready(function() {   
-    var sfID = convertId(window.location.pathname.substring(1,16));
-	var t1='<div class="brandTertiaryBrd pbSubheader tertiaryPalette"><h3>Extra information<span class="titleSeparatingColon">:</span></h3></div>';
-	var t2='<div class="pbSubsection"><table class="detailList" border="0" cellpadding="0" cellspacing="0"><tbody><tr><td class="last labelCol">ID 18 chars</td><td class="dataCol last col02" id="18char">' + sfID + '</td><td>&nbsp;</td><td>&nbsp;</td></tr></tbody></table>';
+$(document).ready(function() {
+    var sfID = convertId(window.location.pathname.substring(1, 16));
 
-	$(".bDetailBlock .pbBody").append(t1 + t2);
+    var t1 = '<div class="brandTertiaryBrd pbSubheader tertiaryPalette">\
+                <h3>Extra information<span class="titleSeparatingColon">:</span></h3>\
+              </div>';
+
+    var t2 = '<div class="pbSubsection">\
+                <table class="detailList" border="0" cellpadding="0" cellspacing="0">\
+                    <tbody>\
+                        <tr>\
+                            <td class="last labelCol">ID 18 chars</td>\
+                            <td class="dataCol last col02" id="18char">' + sfID + '</td>\
+                            <td>&nbsp;</td>\
+                            <td>&nbsp;</td>\
+                        </tr>\
+                    </tbody>\
+                </table>\
+               </div>'; 
+
+    $(".bDetailBlock .pbBody").append(t1 + t2);
 
 
     createLookupLinks();
     $(".goToLink").click(function() {
         fieldId = $(this).siblings("input").attr('id');
-        fieldValue = $("#"+fieldId+"_lkid").val();
-        if(fieldValue != '000000000000000') {
-            window.open("/"+fieldValue, '_blank');
+        fieldValue = $("#" + fieldId + "_lkid").val();
+        if (fieldValue != '000000000000000') {
+            window.open("/" + fieldValue, '_blank');
         }
     });
 });
@@ -21,7 +36,9 @@ function convertId(id) {
     var i, j, flags, alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ012345";
     if (id === null) return id;
     id = id.replace(/\W/g, "");
-    if (id.length != 15) { return id; }
+    if (id.length != 15) {
+        return id;
+    }
 
     for (i = 0; i < 3; i++) {
         flags = 0;
@@ -39,7 +56,7 @@ function isUppercase(c) {
 
 function createLookupLinks() {
     //console.log(chrome.extension.getURL('assets/images/icon.png'));
-    $(".lookupInput").each(function( index, value ) {
+    $(".lookupInput").each(function(index, value) {
         $(this).append('<a href="javascript:return false;" class="goToLink">⇨</a>');
     });
 }
